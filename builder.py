2d698d3c128544a4178a47291acc526ab8032b9a
Dan Head <dan@stoey.net>
2014-08-06 23:01:40 +0000

    Refactored to replace Page with file type handlers

--- builder.py
+++ builder.py
@@ -9,7 +9,137 @@ TEMPLATE_DIR = "templates"
 DATA_DIR = "data"
 OUTPUT_DIR = "static/output"
 
-INCLUDE_PREFIX = '@'
+JSON_INCLUDE_PREFIX = '@'
+
+FILE_HANDLERS = dict(
+    mustache='MustacheHandler',
+)
+
+def get_handler(path):
+    extension = path.rpartition('.')[0]
+    handler_name = FILE_HANDLERS.get(extension, 'DefaultFileHandler')
+    handler_class = globals()[handler_name]
+    return handler_class(path)
+
+
+class DefaultFileHandler(object):
+    def __init__(self, path):
+        self.path = path
+
+    def process(self):
+        # Ensure the parent directories exist
+        out_dir = os.path.dirname(self.output_path)
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
+        # Perform OUTFILE < transform(INFILE)
+        with open(os.path.join(TEMPLATE_DIR, self.path)) as infile:
+            with open(self.output_path, 'wb') as outfile:
+                outfile.write(self.transform(infile))
+
+    def transform(self, f):
+        return f.read()
+
+    def output_name(self):
+        return self.path
+
+    @property
+    def output_path(self):
+        return os.path.join(OUTPUT_DIR, self.output_name())
+
+    def __str__(self):
+        return "%s ----> %s" % (self.path, self.output_name())
+
+class JsonIncluder(object):
+    @staticmethod
+    def is_include(key):
+        return key.startswith(JSON_INCLUDE_PREFIX)
+
+    @staticmethod
+    def include_item(key, context):
+        value = context[key]
+        path, selector = value.partition('#')[::2]
+        with open(path) as f:
+            data = json.load(f)
+        if selector:
+            tree = objectpath.Tree(data)
+            # TODO - object path requires expression be a str
+            # Probably a bug - if so fix it and sand a pull request upstream
+            include = tree.execute(str(selector))
+        else:
+            include = data
+        return key[len(JSON_INCLUDE_PREFIX):],include
+
+    @staticmethod
+    def update(json_obj):
+        updates = {}
+        for key in json_obj.iterkeys():
+            if JsonIncluder.is_include(key):
+                new_key, new_value = JsonIncluder.include_item(key, json_obj)
+                updates[new_key] = new_value
+        for key in updates.iterkeys():
+            del json_obj[JSON_INCLUDE_PREFIX + key]
+            json_obj[key] = updates[key]
+
+class MustacheHandler(DefaultFileHandler):
+    @property
+    def extension(self):
+        return self.path.rpartition('.')[2]
+
+    @property
+    def base_template(self):
+        filename = "%s.%s" % (BASE_PAGE_PATH, self.extension)
+        with open(os.path.join(TEMPLATE_DIR, filename)) as f:
+            return f.read()
+
+    @property
+    def base_context(self):
+        return self._context(BASE_PAGE_PATH + '.json')
+
+    @property
+    def context(self):
+        return self._context(self.context_filename)
+
+    @property
+    def basename(self):
+        self.path.rpartition('.')[0]
+
+    @property
+    def context_filename(self):
+        return self.basename + '.json'
+
+    @staticmethod
+    def _context(filename):
+        context = {}
+        if os.path.exists(filename):
+            if not os.path.isdir(filename):
+                with open(filename) as context_file:
+                    context = json.load(context_file)
+            else:
+                #TODO - just use includes?
+                for sub_filename in os.listdir(filename):
+                    with open(os.path.join(filename, sub_filename)) as context_file:
+                        key = filename.partition('.')[0]
+                        value = json.load(context_file)
+                        context[key] = value
+        JsonIncluder.update(context)
+        return context
+
+
+    def _render(self, template):
+        context = self.context
+        body = pystache.render(template, context)
+        base_context = self.base_context
+        base_context.update(dict(body=body))
+        return pystache.render(self.base_template, base_context)
+
+    def transform(self, f):
+        return self._render(f.read())
+
+    def output_name(self):
+        return self.basename
+
+    def __str__(self):
+        return "%s --[%s]--> %s" % (self.path, self.page.context ,self.output_name())
 
 class Page(object):
     def __init__(self, path):
@@ -27,68 +157,15 @@ class Page(object):
     def output(self):
         return os.path.join(OUTPUT_DIR, self.path)
 
-def include_item(key, context):
-    value = context[key]
-    path, selector = value.partition('#')[::2]
-    with open(path) as f:
-        data = json.load(f)
-    if selector:
-        tree = objectpath.Tree(data)
-        include = tree.execute(str(selector))
-    else:
-        include = data
-    return key[len(INCLUDE_PREFIX):],include
-
-def is_include(key):
-    return key.startswith(INCLUDE_PREFIX)
-
-def process_includes(context):
-    updates = {}
-    for key in context.iterkeys():
-        if is_include(key):
-            new_key, new_value = include_item(key, context)
-            updates[new_key] = new_value
-    for key in updates.iterkeys():
-        del context[INCLUDE_PREFIX + key]
-        context[key] = updates[key]
-
-def get_context(page, extra_context):
-    context = {}
-    if os.path.exists(page.context):
-        if not os.path.isdir(page.context):
-            with open(page.context) as context_file:
-                context = json.load(context_file)
-        else:
-            for filename in os.listdir(page.context):
-                with open(os.path.join(page.context, filename)) as context_file:
-                    key = filename.partition('.')[0]
-                    value = json.load(context_file)
-                    context[key] = value
-    context.update(extra_context)
-    process_includes(context)
-    return context
-
-
-def render_page(page, **extra_context):
-    with open(page.template) as template_file:
-       template = template_file.read()
-    context = get_context(page, extra_context)
-    if page.path != BASE_PAGE_PATH:
-        body = pystache.render(template, context)
-        return render_page(Page(BASE_PAGE_PATH), body=body)
-    else:
-        return pystache.render(template, context)
 
 if __name__ == "__main__":
     for dirpath, dirnames, filenames in os.walk(TEMPLATE_DIR):
         filenames = [fn for fn in filenames if fn[0] not in '._']
         for filename in filenames:
             dirname = dirpath[len(TEMPLATE_DIR):].lstrip('/')
-            path = os.path.join(dirname, filename.rpartition('.')[0])
-            page = Page(path)
-            out_dir = os.path.dirname(page.output)
-            if not os.path.exists(out_dir):
-                os.makedirs(out_dir)
-            with open(page.output, 'w') as output_file:
-                output_file.write(render_page(page))
-            print "%s ---(%s)--> %s" % (page.context, page.template, page.output)
+            extension = filename.rpartition('.')[2]
+            path = os.path.join(dirname, filename)
+            handler = get_handler(path)
+            with open(handler.output_path, 'w') as output_file:
+                handler.process()
+            print handler
